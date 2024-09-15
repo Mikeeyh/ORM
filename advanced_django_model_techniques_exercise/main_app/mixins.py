9af42b3b8d67a578d21f6ed2892c698eb0b4a938
Mike <133797823+Mikeeyh@users.noreply.github.com>
2024-09-15 12:27:55 +0300

    Delete advanced_django_model_techniques_exercise directory

--- advanced_django_model_techniques_exercise/main_app/mixins.py
+++
@@ -1,4 +0,0 @@
-class RechargeEnergyMixin:
-    def recharge_energy(self, amount: int) -> None:
-        self.energy = min(100, self.energy + amount)  # gives us the min number between the two numbers given
-        self.save()
